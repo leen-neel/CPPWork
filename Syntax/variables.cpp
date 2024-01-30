0bd925002c0bbdea81803e20fea8d3c13d58f162
leen-neel <and24903@gmail.com>
2024-01-30 23:52:35 +0530

    vars

---
+++ Syntax/variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x = 5;
+    double y = 5.5;
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
+    bool isTrue = false;
+
+    if (isTrue)
+    {
+        cout << "Sex is good" << endl;
+    }
+}
