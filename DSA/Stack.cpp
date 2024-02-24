ecde81aec915e2f4ed4c60004aecb3f6a583f909
leen-neel <and24903@gmail.com>
2024-02-24 11:01:13 +0530

    wow we stacking

--- DSA/Stack.cpp
+++ DSA/Stack.cpp
@@ -1,16 +1,62 @@
 #include <iostream>
 
+#define MAX 5
+
 using std::cout;
 using std::endl;
 
 class Stack
 {
 
+    int arr[MAX];
     int top;
 
 public:
     Stack()
     {
         top = -1;
+    }
+
+    void push(int data)
+    {
+
+        if (top == MAX - 1)
+        {
+            cout << "Stack is full" << endl;
+            return;
+        }
+
+        top++;
+        arr[top] = data;
+    }
+
+    int pop()
+    {
+
+        if (top == -1)
+        {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+
+        int data = arr[top];
+        top--;
+        return data;
+    }
+
+    void display()
+    {
+
+        if (top == -1)
+        {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+
+        for (int i = 0; i <= top; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
 };
